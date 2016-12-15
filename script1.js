7ab387e6a31b7b58047dae94e7fa13a92b359418
alethea <sachin@alethea.in>
2016-12-15 13:03:39 +0530

    fgfff

---
+++ script1.js
@@ -0,0 +1,8 @@
+var sleep = require('sleep')
+
+for (var i=0;i<3;i++)
+{
+
+	sleep.sleep(3)
+	console.log("javaScript 1 running"+i);
+}
---
+++ script2.js
@@ -0,0 +1,7 @@
+var sleep = require('sleep')
+
+for (var i=0;i<3;i++)
+{
+	sleep.sleep(3)
+	console.log("javaScript 2 running"+i);
+}
